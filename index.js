33fffae37fb191e947f4e286e894149e052df27a
vinoddsce <vinod.dsce@gmail.com>
2019-10-10 18:42:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+// Syntax
+// var variable_name = value;
+
+var a = 100;
+a = true;
+a = '@';
+console.log(a);
+
+// Syntax
+// var obj_name = {
+//     att_name: att_alue,
+
+// };
+
+var benz = {
+    name: "Benz",
+    isPetrol: false,
+    cost: 1240243.90,
+    manDate: '01-JAN-2019'
+}
+
+var bmw = {
+    name: "BMW",
+    isPetrol: false,
+    cost: 1240243.90,
+    manDate: '01-JAN-2019'
+}
+
+console.log(benz);
