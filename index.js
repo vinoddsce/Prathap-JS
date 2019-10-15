df8341ffab74ed1e7bd8cc62fe7d22786e4160b1
vinoddsce <vinod.dsce@gmail.com>
2019-10-15 20:07:04 +0530

    functions

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+console.log(a);
+var a;
+a = 100;
+console.log(a);
+
+
+
+
 // Functions
+
+// Syntax
+// function function_name(optional parameters_list) {
+// .. statements
+// }
+
+
+
+sayHi();
+// displayHi();
+
+function sayHi() {
+    console.log("Hi");
+}
+
+// sayHi();
+
+
+// function sayHi(fName, lName) {
+//     console.log("Hi", fName, lName);
+// }
+
+// sayHi("Vinod", "Kumar");
+// console.log(sayHi);
+
+
+
+var displayHi = function () {
+    console.log("Displaying Hi");
+}
+
+displayHi();
+console.log(displayHi.);
+
+
+
+<blockquote class="twitter-tweet"><p lang="en" dir="ltr">We’re thrilled to announce support for a new set of scenarios! <a href="https://twitter.com/hashtag/Python?src=hash&amp;ref_src=twsrc%5Etfw">#Python</a> and the <a href="https://twitter.com/hashtag/Linux?src=hash&amp;ref_src=twsrc%5Etfw">#Linux</a> Consumption hosting plan are now Generally Available - and the Premium plan now supports Linux and custom containers. Learn more: <a href="https://t.co/fdqujXW4Bi">https://t.co/fdqujXW4Bi</a> <a href="https://t.co/nzoF49tNI2">pic.twitter.com/nzoF49tNI2</a></p>&mdash; Azure Functions (@AzureFunctions) <a href="https://twitter.com/AzureFunctions/status/1163511532417765376?ref_src=twsrc%5Etfw">August 19, 2019</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
 
 
